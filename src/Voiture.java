ba7d44b52c9a3e1f3739cb4da2612845ca67058c
ayoubchehli1977-dev <you@example.com>
2026-04-07 14:43:49 +0200

    mission 3

---
+++ src/Voiture.java
@@ -0,0 +1,18 @@
+
+import javax.lang.model.SourceVersion;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Houda
+ */
+public class Voiture {
+    String marque;
+    String modele;
+    int annee;
+    double prix;
+}
