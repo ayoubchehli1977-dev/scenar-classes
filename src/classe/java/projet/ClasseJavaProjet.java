343544fa99b4384c62b774ff72c857b3d7782d1f
ayoubchehli1977-dev <you@example.com>
2026-04-07 16:46:13 +0200

    missions  6

--- src/classe/java/projet/ClasseJavaProjet.java
+++ src/classe/java/projet/ClasseJavaProjet.java
@@ -14,27 +14,15 @@ public class ClasseJavaProjet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   Voiture v1 = new Voiture();
-   v1.marque = "Peugeot";
-   v1.modele = "208";
-   v1.annee = 2021;
-   v1.prix = 14500.0;
+Pneu p1 = new Pneu(205, 55, 16, "Ete");
+Voiture v1 = new Voiture("Peugeot", "208", 2021, 14500.0, p1);
+Voiture v2 = new Voiture("Toyota", "Yaris", 2015, 9800.0, p1); 
+Voiture v3 = new Voiture("Renault", "clio", 2000, 3500.0, p1); 
    
-   System.out.println("Voiture 1 : " + v1.marque + " " + v1.modele + " " + v1.annee + " "+v1.prix + "$");
+   System.out.println("Voiture 1 : " + v1.marque + " " + v1.modele + " " + v1.annee + " "+v1.prix + " " + v1.pneu.type);
+   System.out.println("Voiture 1 : " + v2.marque + " " + v2.modele + " " + v2.annee + " "+v2.prix + " "+  v2.pneu.type);
+   System.out.println("Voiture 2 : " + v3.marque + " " + v3.modele + " " + v3.annee + " "+v3.prix + " " + v3.pneu.type);
    
-    Voiture v2 = new Voiture();
-   v2.marque = "Toyota";
-   v2.modele = "Yaris";
-   v2.annee = 2015;
-   v2.prix = 9800;
-   System.out.println("Voiture 2 : " + v2.marque + " " + v2.modele + " " + v2.annee + " "+v2.prix + "$");
-   
-   Voiture v3 = new Voiture();
-   v3.marque = "Renault";
-   v3.modele = "clio";
-   v3.annee = 2000;
-   v3.prix = 3500;
-   System.out.println("Voiture 2 : " + v3.marque + " " + v3.modele + " " + v3.annee + " "+v3.prix + "$");
    
    
    
---
+++ src/classe/java/projet/Pneu.java
@@ -0,0 +1,31 @@
+package classe.java.projet;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Houda
+ */
+public class Pneu {
+    int hauteur;
+    int largeur;
+    int diametre;
+    String type;
+    
+    
+    public Pneu(int largeur , int hauteur , int diametre , String type){
+        this.largeur = largeur;
+        this.diametre = diametre;
+        this.hauteur = hauteur;
+        this.type = type;
+        
+        
+        
+    }
+    
+    
+    
+}
